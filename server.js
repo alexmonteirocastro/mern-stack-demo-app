3b6e80c37485b0771680c8cb2334c9b60f4b3624
Alexandre Castro <alex.monteiro@seznam.cz>
2018-08-04 19:29:41 +0200

    preparing for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -22,10 +23,16 @@ mongoose.connect(db, { useNewUrlParser: true })
 // Use routes
 app.use('/api/items', items);
 
-const port = process.env.PORT || 5000;
+// Serve static ('Build') assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
 
-/* if (!module.parent) {
-    app.listen(config.PORT);
-} */
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
